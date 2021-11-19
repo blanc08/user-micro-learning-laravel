7bc7031039c9d984ed2605b61bea8bbec13fe418
blanc20 <bagusoktaviadi1@gmail.com>
2021-11-19 08:28:07 +0700

    asset install

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $response = Http::get('http://localhost:3000/key');
+    return $response;
+});
+
+// Route::resource('/api/v1/products', 'ProductController');
+Route::resource('users', UserController::class);
